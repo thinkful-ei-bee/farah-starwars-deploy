9113f8d3694edf719222e543586ed7a098588da0
Farah Student <farah.gus@hotmail.com>
2019-03-29 13:23:25 -0400

    working results list

--- src/App.js
+++ src/App.js
@@ -10,25 +10,63 @@ class App extends Component {
     error: null,
   }
 
-  resultsList = () => {
-    console.log(this.state.characters)
-    let searchResults = (!this.state.characters) ? [] : this.state.characters
-    searchResults = Object.keys(this.state.characters.map(character => 
-      <div>
-        <h3>{character}</h3>
-      </div>))
+  updateSearch = (searchTerm) => {
+    this.setState({searchTerm});
+    console.log(searchTerm)
   }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let url = `https://swapi.co/api/people/?search=`+this.state.searchTerm
+    //get data from api
+    fetch(url)
+      .then(res => res.json())
+      .then (data => {
+        // add data from api to characters
+        this.setState({
+          characters: data 
+        })
+      // console.log(url)
+      // console.log(this.state.characters.results)
+      })
+      .catch(err => console.log(err));
+  }
+
+  // searchResults = () => {
+  //   const results = this.state.characters.results.map(character => 
+  //     <div>
+  //       <h3>{character}</h3>
+  //     </div>
+  //     )}
 
 
   render() {
+    let resultsList = (!this.state.characters.results) ? [] : this.state.characters.results
+    console.log(resultsList)
+    resultsList = resultsList.map(character => 
+      <div>
+        <h3>{character.name}</h3>
+      </div>)
+      
     return (
       <main className="App">
         <h1>Search for a Star Wars Character:</h1>
         <div className="search">
-          <SearchForm />
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <label htmlFor="searchStarwars">
+            Search: {' '}
+          </label>
+          <input 
+            onChange={e => this.updateSearch(e.target.value)}
+            placeholder="e.g. Luke Skywalker"
+            id="searchStarwars" 
+            type="text"/>{' '}
+          <button type="submit">Go!</button>
+        </form>
         </div>
+
         <div className="results">
-          <ResultsList />
+          {resultsList}
         </div>
       </main>
     );
--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -7,21 +7,22 @@ export default class SearchForm extends React.Component{
   }
 
   updateSearch = (searchTerm) => {
-    console.log(searchTerm)
     this.setState({searchTerm});
+    console.log(searchTerm)
   }
 
   handleSubmit = e => {
     e.preventDefault();
+    let url = `https://swapi.co/api/people/?search=`+this.state.searchTerm
     //get data from api
-    fetch(`https://swapi.co/api/people/?search=`+this.searchTerm)
+    fetch(url)
       .then(res => res.json())
       .then (data => {
         // add data from api to characters
         this.setState({
           characters: data 
         })
-      // console.log(this.characters)
+      console.log(url)
       })
       .catch(err => console.log(err));
   }
@@ -29,17 +30,19 @@ export default class SearchForm extends React.Component{
   
   render(){
     return (
-      <form onSubmit={e => this.handleSubmit(e)}>
-          <label htmlFor="searchStarwars">
-            Search: {' '}
-          </label>
-          <input 
-            onChange={e => this.updateSearch(e.target.value)}
-            placeholder="e.g. Luke Skywalker"
-            id="searchStarwars" 
-            type="text"/>{' '}
-          <button type="submit">Go!</button>
-        </form>
+      <div className="search-form">
+        <form onSubmit={e => this.handleSubmit(e)}>
+            <label htmlFor="searchStarwars">
+              Search: {' '}
+            </label>
+            <input 
+              onChange={e => this.updateSearch(e.target.value)}
+              placeholder="e.g. Luke Skywalker"
+              id="searchStarwars" 
+              type="text"/>{' '}
+            <button type="submit">Go!</button>
+          </form>
+        </div>
     )
   }
 }
